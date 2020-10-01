2c94695beee1b3c77040c7faabf094bfefb9037f
Ricardo <ricardo.a.nava@gmail.com>
2020-09-30 23:00:49 -0700

    Add graphQL endpoint

--- App.js
+++ App.js
@@ -2,12 +2,24 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Platform, AppRegistry } from 'react-native';
 import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
-import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink
+} from '@apollo/client';
 import { RootNavigator } from './navigation/rootNavigator';
+
+// Instantiate required constructor fields
+const cache = new InMemoryCache();
+const link = createHttpLink({
+  uri: 'https://pkdx-apollo-engine.herokuapp.com/'
+});
 
 // Create the client as outlined in the setup guide
 const client = new ApolloClient({
-  cache: new InMemoryCache()
+  cache,
+  link
 });
 
 const theme = {
